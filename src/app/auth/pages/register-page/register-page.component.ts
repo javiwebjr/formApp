756cbf55f070fb93493e05c3d4bcfc6ded6530fd
javiwebjr <urrutia_987@hotmail.com>
2024-01-25 16:31:28 -0600

    Reactive Forms

---
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+// import { cantBeStrider, emailPattern, firstNameAndLastnamePattern } from '../../../shared/validators/validators';
+import { ValidatorsService } from '../../../shared/services/validators.service';
+import { EmailValidatorService } from '../../../shared/validators/email-validator.service';
+
+@Component({
+  templateUrl: './register-page.component.html',
+  styles: ``
+})
+export class RegisterPageComponent {
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.pattern(this.validatorService.firstNameAndLastnamePattern)]],
+    // email: ['', [Validators.required, Validators.pattern(this.validatorService.emailPattern)], [new EmailValidatorService()]],
+    email: ['', [Validators.required, Validators.pattern(this.validatorService.emailPattern)], [this.emailValidator]],
+    username: [ '', [ Validators.required, this.validatorService.cantBeStrider ] ],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    password2: ['', [Validators.required]],
+  }, {
+    validators: [
+      this.validatorService.arePasswordEquals('password', 'password2')
+    ]
+  });
+
+  constructor(private fb: FormBuilder,
+    private validatorService:ValidatorsService,
+    private emailValidator: EmailValidatorService
+  ){}
+
+  isValidField(field: string){
+    //TODO: Obtener validator desde un servicio
+    return this.validatorService.isValidField(this.myForm, field);
+  }
+
+  onSubmit(){
+    this.myForm.markAllAsTouched();
+  }
+
+}
